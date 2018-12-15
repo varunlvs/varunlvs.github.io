9077f5f29531646c8c7eca28817ef8e46cde5142
varunlvs <42297561+varunlvs@users.noreply.github.com>
2018-12-15 22:30:21 +0530

    GameBerry Labs LVS Update

--- GameBerryLabs/js/script.js
+++ GameBerryLabs/js/script.js
@@ -1,3 +1,5 @@
+var displayCover;
+displayCover = false;
 /*======================================================
 						Preloader
 ======================================================*/
@@ -6,3 +8,28 @@ $(window).on('load',function(){
 	$('#status').fadeOut();
 	$('#preloader').delay(350).fadeOut('slow');
 });
+
+// CUSTOM
+init();
+
+function init() {
+	document.getElementById('cover').style.display = 'none';
+}
+
+document.getElementById('toggle').addEventListener('click',function(){
+
+	toggleDisplay();
+});
+
+function toggleDisplay() {
+    displayCover === 0 ? displayCover = 1 : displayCover = 0;
+
+    console.log(displayCover);
+
+    if(displayCover==0)
+		document.getElementById('cover').style.display = 'block';
+	else
+		document.getElementById('cover').style.display = 'none';
+
+
+}
