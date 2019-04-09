00ac926210b5cb84891e624f8a3d687be8fa93f3
varunlvs <42297561+varunlvs@users.noreply.github.com>
2019-04-09 20:03:12 +0530

    update

--- WritersPlay/js/custom.js
+++ WritersPlay/js/custom.js
@@ -45,6 +45,25 @@ $(function () {
 		  }
 		}
     });
+
+    $("#clients-list").owlCarousel({
+        items: 3,
+        autoplay: true,
+        smartSpeed: 700,
+        loop: true,
+        autoplayHoverPause: true,
+        responsive: {
+          0: {
+            items: 1
+          },
+          480: {
+            items: 2
+          },
+          768: {
+            items: 3
+          }
+        }
+    });
 });
 
 
